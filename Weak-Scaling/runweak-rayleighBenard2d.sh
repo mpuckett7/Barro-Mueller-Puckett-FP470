c6475a855efedf48284fee0533a11c489c584356
Beau Mueller <muellebr@dukes.jmu.edu>
2025-04-18 17:01:43 -0400

    weak scaling test script for rayleigh w/ output files

--- Weak-Scaling/runweak-rayleighBenard2d.sh
+++ Weak-Scaling/runweak-rayleighBenard2d.sh
@@ -9,6 +9,9 @@ make clean; make
 cd examples/thermal/rayleighBenard2d
 make
 
-for t in 2 5 10 20; do
-    OMP_NUM_THREADS=16 salloc ./rayleighBenard2d -r $t &> weak_ray_r=$t.txt
+thread_count=(1 2 4 8 16 32)
+sim_res=(2 3 4 5 6 7)
+
+for t in 0 1 2 3 4 5; do
+    OMP_NUM_THREADS=${thread_count[$t]} salloc ./rayleighBenard2d -r ${sim_res[$t]} &> weak_ray_r=$t.txt
 done
